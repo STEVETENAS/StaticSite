eed0fdb8ab9f1cb7a41c47e5b25a432895014669
STEVE TENAS <stevetenadjang@gmail.com>
2021-07-31 19:38:41 +0100

    - correction of responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('tailwind-scrollbar-hide'),
+    ],
 }
